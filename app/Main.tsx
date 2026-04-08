c58a6a97d80b52318cb36d789e100c2ce2c0bcf5
Romain Baugue <romain.baugue@elwinar.com>
2026-04-08 23:31:06 +0200

    add a load world form in the app

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,3 +1,38 @@
+import { type ChangeEvent, type SubmitEvent, useState } from "react";
+
 export function Main() {
-	return <h1>pocket world sim</h1>;
+	const [addr, setAddr] = useState<string>("http://localhost:8765");
+	// biome-ignore lint/suspicious/noExplicitAny: not used yet
+	const [world, setWorld] = useState<any>({});
+
+	function handleAddrChange(e: ChangeEvent<HTMLInputElement>) {
+		setAddr(e.target.value);
+	}
+
+	function handleFormSubmit(e: SubmitEvent<HTMLFormElement>) {
+		e.preventDefault();
+		fetchWorld(addr).then(setWorld);
+		return false;
+	}
+
+	return (
+		<>
+			<h1>pocket world sim</h1>
+			<form onSubmit={handleFormSubmit}>
+				<input type="text" value={addr} onChange={handleAddrChange} />
+				<button type="submit">load</button>
+			</form>
+			<pre>{JSON.stringify(world)}</pre>
+		</>
+	);
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: no parsing of the payload yet
+async function fetchWorld(addr: string): Promise<any> {
+	return fetch(`${addr}/world`).then((res) => {
+		if (!res.ok) {
+			throw new Error("fetching world failed");
+		}
+		return res.json();
+	});
 }
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,6 @@ import type { UserConfig } from "vite";
 
 export default {
 	server: {
-		port: 8001,
+		port: 8766,
 	},
 } satisfies UserConfig;
